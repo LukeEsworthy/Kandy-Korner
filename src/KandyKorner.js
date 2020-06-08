0f60af81f8fe8ff5971721154c7298adb35b0a0a
Luke Esworthy <lukesworthy@gmail.com>
2020-06-08 11:31:41 -0500

    initial commit, created file structure

---
+++ src/KandyKorner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./KandyKorner.css";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Houchens' Kandy Kounter
+      </h1>
+    </div>
+  );
+}
+
+export default App;
